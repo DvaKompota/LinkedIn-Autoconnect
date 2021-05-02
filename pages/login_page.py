fa591808a9b388a3cbe539a28ba92ecfbfab6b19
Sergey Kolokolov <serg.a.bells@gmail.com>
2021-05-02 13:26:07 -0700

    adds login_page.py with some basic locators

---
+++ pages/login_page.py
@@ -0,0 +1,8 @@
+login_url = "https://www.linkedin.com/"
+
+############################## LOCATORS ##############################
+email_field = '//input[@id="session_key"]'
+pass_field = '//input[@id="session_password"]'
+submit_button = '//button[@type="submit"]'
+avatar = '//img[contains(@alt, "Photo of ")]'
+my_network = '//a[@href="/mynetwork/"]'
