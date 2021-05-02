3cdfbff2a13ae6bf2dbc230637f3ad12ffd06f24
Sergey Kolokolov <serg.a.bells@gmail.com>
2021-05-02 15:07:11 -0700

    moves login stuff to LoginPage object and adds get_element() to BasePage class

--- pages/base_page.py
+++ pages/base_page.py
@@ -8,23 +8,26 @@ class BasePage:
         self.driver = get_driver()
         self.driver_wait = 10
 
+    def get_element(self, locator_name):
+        return getattr(self, locator_name)
+
     def open_url(self, url):
-        self.driver.get(url)
+        self.driver.get(self.get_element(url))
 
     def close_browser(self):
         self.driver.close()
 
     def wait_element_displayed(self, locator):
         wait = ui.WebDriverWait(self.driver, self.driver_wait)
-        wait.until(lambda driver: self.driver.find_element_by_xpath(locator).is_displayed())
+        wait.until(lambda driver: self.driver.find_element_by_xpath(self.get_element(locator)).is_displayed())
 
     def is_displayed(self, locator):
-        return self.driver.find_element_by_xpath(locator).is_displayed()
+        return self.driver.find_element_by_xpath(self.get_element(locator)).is_displayed()
 
     def click(self, locator):
         self.wait_element_displayed(locator)
-        self.driver.find_element_by_xpath(locator).click()
+        self.driver.find_element_by_xpath(self.get_element(locator)).click()
 
     def enter_text(self, locator, text):
         self.wait_element_displayed(locator)
-        self.driver.find_element_by_xpath(locator).send_keys(text)
+        self.driver.find_element_by_xpath(self.get_element(locator)).send_keys(text)
--- pages/login_page.py
+++ pages/login_page.py
@@ -1,8 +1,13 @@
-login_url = "https://www.linkedin.com/"
+from pages.base_page import BasePage
 
-############################## LOCATORS ##############################
-email_field = '//input[@id="session_key"]'
-pass_field = '//input[@id="session_password"]'
-submit_button = '//button[@type="submit"]'
-avatar = '//img[contains(@alt, "Photo of ")]'
-my_network = '//a[@href="/mynetwork/"]'
+
+class LoginPage(BasePage):
+
+    login_url = "https://www.linkedin.com/"
+
+    # ============================== LOCATORS ============================== #
+    email_field = '//input[@id="session_key"]'
+    pass_field = '//input[@id="session_password"]'
+    submit_button = '//button[@type="submit"]'
+    avatar = '//img[contains(@alt, "Photo of ")]'
+    my_network = '//a[@href="/mynetwork/"]'
