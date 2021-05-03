ee8871caba1a041287ea98790a9846d5b6f89303
Sergey Kolokolov <serg.a.bells@gmail.com>
2021-05-02 19:52:53 -0700

    adds a list of interesting companies to connect with

---
+++ data/companies.py
@@ -0,0 +1,15 @@
+companies = [
+    "Boston Dynamics",
+    "Deep Mind",
+    "Open AI",
+    "Neurolink",
+    "SpaceX",
+    "Relativity Space",
+    "Rocket Lab",
+    "Tesla",
+    "Arrival",
+    "Zoox",
+    "Rivian",
+    "Waymo",
+    "Apple"
+]
