619438c6beb975f9d0fad01d5d7d46ff12a42e35
Sergey Kolokolov <serg.a.bells@gmail.com>
2021-05-02 19:37:28 -0700

    adds couple wait_element methods to BasePage and makes get_element() metod more universal

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,7 @@
 import selenium.webdriver.support.ui as ui
+from selenium.common.exceptions import StaleElementReferenceException
+from time import time
+from time import sleep
 
 
 class BasePage:
@@ -8,7 +11,10 @@ class BasePage:
         self.driver_wait = 10
 
     def get_element(self, locator_name):
-        return getattr(self, locator_name)
+        try:
+            return getattr(self, locator_name)
+        except AttributeError:
+            return locator_name
 
     def open_url(self, url):
         self.driver.get(self.get_element(url))
@@ -20,8 +26,28 @@ class BasePage:
         wait = ui.WebDriverWait(self.driver, self.driver_wait)
         wait.until(lambda driver: self.driver.find_element_by_xpath(self.get_element(locator)).is_displayed())
 
+    def wait_element_not_displayed(self, locator):
+        end_time = time() + self.driver_wait
+        while time() < end_time:
+            if not self.is_displayed(locator):
+                break
+
+    def wait_element_selected(self, locator):
+        end_time = time() + self.driver_wait
+        while time() < end_time:
+            try:
+                if self.is_selected(locator):
+                    break
+            except StaleElementReferenceException:
+                sleep(0.5)
+                if self.is_selected(locator):
+                    break
+
     def is_displayed(self, locator):
         return self.driver.find_element_by_xpath(self.get_element(locator)).is_displayed()
+
+    def is_selected(self, locator):
+        return "selected" in self.driver.find_element_by_xpath(self.get_element(locator)).get_attribute("class")
 
     def click(self, locator):
         self.wait_element_displayed(locator)
